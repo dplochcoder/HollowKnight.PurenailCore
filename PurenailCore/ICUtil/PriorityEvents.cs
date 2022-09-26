26b1edb893579c2b79fe816d79babb0bc2c9e92f
dplochcoder <daniel.ploch.coder@gmail.com>
2022-09-26 17:11:42 -0400

    Add events for after SceneManager start.

--- PurenailCore/ICUtil/PriorityEvents.cs
+++ PurenailCore/ICUtil/PriorityEvents.cs
@@ -11,20 +11,27 @@ namespace PurenailCore.ICUtil
     {
         public static void Setup()
         {
-            On.SceneManager.Start += HandleBeforeSceneManagerStart;
+            On.SceneManager.Start += OnSceneManagerStart;
         }
 
         public delegate void UpdateSceneManager(SceneManager sm);
+
         public static readonly PriorityEvent<UpdateSceneManager> BeforeSceneManagerStart = new(out _beforeSceneManagerStartOwner);
         private static readonly PriorityEvent<UpdateSceneManager>.IPriorityEventOwner _beforeSceneManagerStartOwner;
+        public static readonly PriorityEvent<UpdateSceneManager> AfterSceneManagerStart = new(out _afterSceneManagerStartOwner);
+        private static readonly PriorityEvent<UpdateSceneManager>.IPriorityEventOwner _afterSceneManagerStartOwner;
 
-        private static void HandleBeforeSceneManagerStart(On.SceneManager.orig_Start orig, SceneManager self)
+        private static void OnSceneManagerStart(On.SceneManager.orig_Start orig, SceneManager self)
         {
             foreach (var updater in _beforeSceneManagerStartOwner.GetSubscribers())
             {
                 updater(self);
             }
             orig(self);
+            foreach (var updater in _afterSceneManagerStartOwner.GetSubscribers())
+            {
+                updater(self);
+            }
         }
     }
 }
