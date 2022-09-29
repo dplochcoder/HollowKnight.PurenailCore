a83f6d97bddcdf915fd4ef48ee97af1680684a5d
dplochcoder <daniel.ploch.coder@gmail.com>
2022-09-29 01:22:06 -0400

    Define a Preloader utility

---
+++ PurenailCore/ModUtil/Preloader.cs
@@ -0,0 +1,53 @@
+﻿using PurenailCore.SystemUtil;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PurenailCore.ModUtil
+{
+    // Add these as members on a subclass of Preloader. Then, hook Preloader into the related Mod class methods.
+    public class PreloadedObject
+    {
+        public PreloadedObject(Preloader preloader, string sceneName, string objName) => preloader.Add(this, sceneName, objName);
+
+
+        private GameObject gameObject;
+
+        public void Init(GameObject gameObject) => this.gameObject = gameObject;
+
+        public GameObject Instantiate() => GameObject.Instantiate(gameObject);
+    }
+
+    public class Preloader
+    {
+        private Dictionary<string, Dictionary<string, List<PreloadedObject>>> preloads;
+
+        public void Add(PreloadedObject pObj, string sceneName, string objName) => preloads.GetOrCreateNew(sceneName).GetOrCreateNew(objName).Add(pObj);
+
+        public List<(string, string)> GetPreloadNames()
+        {
+            List<(string, string)> l = new();
+            foreach (var e in preloads)
+            {
+                foreach (var objName in e.Value.Keys)
+                {
+                    l.Add((e.Key, objName));
+                }
+            }
+            return l;
+        }
+
+        public void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+        {
+            foreach (var e1 in preloads)
+            {
+                foreach (var e2 in e1.Value)
+                {
+                    var obj = preloadedObjects[e1.Key][e2.Key];
+                    e2.Value.ForEach(p => p.Init(obj));
+                }
+
+            }
+        }
+    }
+}
---
+++ PurenailCore/SystemUtil/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurenailCore.SystemUtil
+{
+    public static class Extensions
+    {
+        public static V GetOrCreateNew<K,V>(this IDictionary<K, V> self, K key) where V : new()
+        {
+            if (self.TryGetValue(key, out V value)) return value;
+
+            value = new();
+            self[key] = value;
+            return value;
+        }
+    }
+}
