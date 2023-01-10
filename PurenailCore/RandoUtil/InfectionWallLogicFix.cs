b554aa763d22d0fa9332ccf05eeeda3b6ddcd156
dplochcoder <daniel.ploch.coder@gmail.com>
2023-01-09 20:10:37 -0500

    Fix logic bug with MoreDoors+Darkness

--- PurenailCore/RandoUtil/InfectionWallLogicFix.cs
+++ PurenailCore/RandoUtil/InfectionWallLogicFix.cs
@@ -22,8 +22,10 @@ namespace PurenailCore.RandoUtil
             if (!tester(gs) || lmb.LogicLookup.ContainsKey(SENTINEL)) return;
             lmb.AddLogicDef(new(SENTINEL, "TRUE"));
 
-            lmb.DoLogicEdit(new("Crossroads_06[right1]", "ORIG + (ROOMRANDO | Crossroads_06[right1] | Crossroads_10[right1])"));
-            lmb.DoLogicEdit(new("Crossroads_10[left1]", "ORIG + (ROOMRANDO | Crossroads_10[left1] | Crossroads_06[door1] | Crossroads_33[top1])"));
+            const string bothSidesProxy = "(Crossroads_06[door1] | Crossroads_06[left1]) + Defeated_False_Knight";
+
+            lmb.DoLogicEdit(new("Crossroads_06[right1]", $"ORIG + (ROOMRANDO | ${bothSidesProxy})"));
+            lmb.DoLogicEdit(new("Crossroads_10[left1]", $"ORIG + (ROOMRANDO | ${bothSidesProxy})"));
         }
     }
 }
