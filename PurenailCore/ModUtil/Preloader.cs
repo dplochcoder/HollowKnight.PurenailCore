1433db251147a925eeb79d224363db8084c510a3
dplochcoder <daniel.ploch.coder@gmail.com>
2023-03-25 17:16:40 -0400

    Implement prefab preload

--- PurenailCore/ModUtil/Preloader.cs
+++ PurenailCore/ModUtil/Preloader.cs
@@ -1,5 +1,6 @@
 ﻿using PurenailCore.SystemUtil;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -8,17 +9,48 @@ using UnityEngine;
 
 namespace PurenailCore.ModUtil
 {
+    public interface IPreload
+    {
+        public string SceneName { get; }
+        public string ObjectName { get; }
+        public bool IsPrefab { get; }
+    }
+
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
-    public class Preload : Attribute
+    public class Preload : Attribute, IPreload
     {
         public readonly string SceneName;
         public readonly string ObjectName;
-        
+
         public Preload(string sceneName, string objectName)
         {
             this.SceneName = sceneName;
             this.ObjectName = objectName;
         }
+
+        public bool IsPrefab => false;
+
+        string IPreload.SceneName => SceneName;
+
+        string IPreload.ObjectName => ObjectName;
+    }
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
+    public class PrefabPreload : Attribute, IPreload
+    {
+        public readonly string SceneName;
+        public readonly string ObjectName;
+        public PrefabPreload(string sceneName, string objectName)
+        {
+            this.SceneName = sceneName;
+            this.ObjectName = objectName;
+        }
+
+        public bool IsPrefab => true;
+
+        string IPreload.SceneName => SceneName;
+
+        string IPreload.ObjectName => ObjectName;
     }
 
     // Subclass this class with your own Preloader type.
@@ -32,6 +64,7 @@ namespace PurenailCore.ModUtil
     {
         private class Target
         {
+            public bool isPrefab;
             public List<FieldInfo> Fields = new();
             public List<PropertyInfo> Props = new();
 
@@ -52,26 +85,34 @@ namespace PurenailCore.ModUtil
 
                 _targets = new();
 
-                var props = GetType().GetProperties().Where(p => p.IsDefined(typeof(Preload), false)).ToList();
+                var props = GetType().GetProperties()
+                    .Where(p => p.IsDefined(typeof(Preload), false) || p.IsDefined(typeof(PrefabPreload), false))
+                    .ToList();
                 foreach (var prop in props)
                 {
-                    var preload = prop.GetCustomAttribute<Preload>();
+                    var preload = (IPreload)prop.GetCustomAttribute<Preload>() ?? (IPreload)prop.GetCustomAttribute<PrefabPreload>();
                     if (prop.PropertyType != typeof(GameObject))
                     {
                         throw new ArgumentException($"Improper use of [Preload] attribute: Expected GameObject, but got {prop.PropertyType} on {prop.Name}");
                     }
-                    _targets.GetOrAddNew(preload.SceneName).GetOrAddNew(preload.ObjectName).Props.Add(prop);
+                    var target = _targets.GetOrAddNew(preload.SceneName).GetOrAddNew(preload.ObjectName);
+                    target.isPrefab = preload.IsPrefab;
+                    target.Props.Add(prop);
                 }
 
-                var fields = GetType().GetFields().Where(f => f.IsDefined(typeof(Preload), false)).ToList();
+                var fields = GetType().GetFields()
+                    .Where(f => f.IsDefined(typeof(Preload), false) || f.IsDefined(typeof(PrefabPreload), false))
+                    .ToList();
                 foreach (var field in fields)
                 {
-                    var preload = field.GetCustomAttribute<Preload>();
+                    var preload = (IPreload)field.GetCustomAttribute<Preload>() ?? (IPreload)field.GetCustomAttribute<PrefabPreload>();
                     if (field.FieldType != typeof(GameObject))
                     {
                         throw new ArgumentException($"Improper use of [Preload] attribute: Expected GameObject, but got {field.FieldType} on {field.Name}");
                     }
-                    _targets.GetOrAddNew(preload.SceneName).GetOrAddNew(preload.ObjectName).Fields.Add(field);
+                    var target = _targets.GetOrAddNew(preload.SceneName).GetOrAddNew(preload.ObjectName);
+                    target.isPrefab = preload.IsPrefab;
+                    target.Fields.Add(field);
                 }
 
                 return _targets;
@@ -83,9 +124,53 @@ namespace PurenailCore.ModUtil
             List<(string, string)> l = new();
             foreach (var e in Targets)
             {
-                foreach (var objName in e.Value.Keys)
+                foreach (var e2 in e.Value)
                 {
-                    l.Add((e.Key, objName));
+                    if (!e2.Value.isPrefab)
+                    {
+                        l.Add((e.Key, e2.Key));
+                    }
+                }
+            }
+            return l;
+        }
+
+        public List<(string, Func<IEnumerator>)> PreloadSceneHooks()
+        {
+            List<(string, Func<IEnumerator>)> l = new();
+            foreach (var e in Targets)
+            {
+                var sceneName = e.Key;
+                List<string> objNames = new();
+                foreach (var e2 in e.Value)
+                {
+                    var objName = e2.Key;
+                    var target = e2.Value;
+                    if (target.isPrefab)
+                    {
+                        objNames.Add(objName);
+                    }
+                }
+
+                if (objNames.Count > 0)
+                {
+                    IEnumerator SaveAssets()
+                    {
+                        var sceneMap = Targets[sceneName];
+                        foreach (GameObject go in Resources.FindObjectsOfTypeAll<GameObject>())
+                        {
+                            if (sceneMap.TryGetValue(go.name, out var target))
+                            {
+                                var prefab = UnityEngine.Object.Instantiate(go);
+                                UnityEngine.Object.DontDestroyOnLoad(prefab);
+                                prefab.SetActive(false);
+                                prefab.name = go.name;
+                                target.SetValue(this, prefab);
+                            }
+                        }
+                        yield break;
+                    }
+                    l.Add((sceneName, SaveAssets));
                 }
             }
             return l;
