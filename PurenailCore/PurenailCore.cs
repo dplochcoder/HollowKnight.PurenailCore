51130e22876ac10be4769c3e6d6bd6f97cafe295
dplochcoder <daniel.ploch.coder@gmail.com>
2024-02-27 00:43:21 -0500

    Remove hk7y surprises

--- PurenailCore/PurenailCore.cs
+++ PurenailCore/PurenailCore.cs
@@ -1,7 +1,5 @@
 ﻿using Modding;
 using PurenailCore.ICUtil;
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -17,13 +15,5 @@ public class PurenailCore : Mod
 
     public PurenailCore() : base("Purenail Core") { Instance = this; }
 
-    public override List<(string, string)> GetPreloadNames() => Preloader.Instance.GetPreloadNames();
-
-    public override (string, Func<IEnumerator>)[] PreloadSceneHooks() => Preloader.Instance.PreloadSceneHooks();
-
-    public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
-    {
-        Preloader.Instance.Initialize(preloadedObjects);
-        PriorityEvents.Setup();
-    }
+    public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects) => PriorityEvents.Setup();
 }
