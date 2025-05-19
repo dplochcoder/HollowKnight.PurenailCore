7a4a502752cf979fd60a74fab3560782046b72ce
dplochcoder <daniel.ploch.coder@gmail.com>
2025-05-19 14:33:34 -0400

    Implement DamageEnemies remover

---
+++ PurenailCore/ICUtil/DamageEnemiesRemoverDeployer.cs
@@ -0,0 +1,19 @@
+﻿using ItemChanger;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PurenailCore.ICUtil;
+
+internal record DamageEnemiesRemoverDeployer : IDeployer
+{
+    public string SceneName { get; set; } = "";
+
+    public void OnSceneChange(Scene to)
+    {
+        foreach (var fsm in Object.FindObjectsOfType<PlayMakerFSM>())
+        {
+            if (fsm.FsmName == "damages_enemy") Object.Destroy(fsm);
+        }
+        foreach (var damageEnemies in Object.FindObjectsOfType<DamageEnemies>()) Object.Destroy(damageEnemies);
+    }
+}
